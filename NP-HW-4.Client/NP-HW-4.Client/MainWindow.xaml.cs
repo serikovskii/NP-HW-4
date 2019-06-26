376c5ec55d4cd5f0e2b115ee6fb9c9e1f11b7f0f
Azat <46015824+serikovskii@users.noreply.github.com>
2019-06-26 23:54:14 +0600

    недоделана

---
+++ NP-HW-4.Client/NP-HW-4.Client/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NP_HW_4.Client
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetScreenButton(object sender, RoutedEventArgs e)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            EndPoint serverEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+
+            server.SendTo(Encoding.ASCII.GetBytes("getScreen"), serverEP);
+            byte[] buffer = new byte[8192 * 1024];
+            while (true)
+            {
+                server.ReceiveFrom(buffer, ref serverEP);
+                if (buffer.Length > 0)
+                {
+                    MemoryStream stream = new MemoryStream(buffer);
+                    BitmapImage imageReceive = new BitmapImage();
+                    imageReceive.StreamSource = stream;
+                    image.Source = imageReceive;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ NP-HW-4.Server/NP-HW-4.Server/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace NP_HW_4.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var server = new Socket(
+                AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            var serverEP = new IPEndPoint(IPAddress.Any, 12345);
+            server.Bind(serverEP);
+            EndPoint remoteEP = new IPEndPoint(0, 0);
+            var buffer = new byte[1024 * 4];
+            var image = new byte[1000000];
+
+            while (true)
+            {
+                int reciveSize = server.ReceiveFrom(buffer, ref remoteEP);
+                if (Encoding.UTF8.GetString(buffer, 0, reciveSize) == "getScreen")
+                {
+                    Bitmap screen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                    Graphics graphics = Graphics.FromImage(screen as Image);
+                    graphics.CopyFromScreen(0, 0, 0, 0, screen.Size);
+                    screen.Save("123.jpeg");
+
+                    Console.WriteLine("screen done");
+
+                    MemoryStream stream = new MemoryStream();
+                    screen.Save(stream, System.Drawing.Imaging.ImageFormat.Bmp);
+                    var size = stream.Length;
+                    image = stream.GetBuffer();
+                    server.SendTo(image, remoteEP);
+
+                }
+            }
+        }
+
+    }
+}
